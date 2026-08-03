2a9c7be8f17f11784e09e144b764938171d4241b
Alameen Azad <alameen.azad@protonmail.com>
2026-08-03 21:13:56 +0300

    feat: add vorpruefung-ticket content-type scaffold

---
+++ src/api/vorpruefung-ticket/controllers/vorpruefung-ticket.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * vorpruefung-ticket controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::vorpruefung-ticket.vorpruefung-ticket");
